55aa508ee5c445da6f7be2007d31baa2a268be37
adhara monzon <adhara.monzonvarillas@everis.nttdata.com>
2022-08-25 13:32:28 +0200

    add basic lazyload

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes, NavLink } from "react-router-dom";
 import  logo  from "../logo.svg";
 import { routes } from '../routes/routes';
@@ -5,31 +6,33 @@ import { routes } from '../routes/routes';
 export const Navigation = () => {
 
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React logo"/>
-          <ul>
-           {
-           routes.map(route => (
-            <li key={route.name}>
-            <NavLink to={route.to} className={({isActive}) => isActive ? 'nav-active' : ''}>
-              {route.name}
-            </NavLink>
-            </li>
-            ))
-          }
-          </ul>
-        </nav>
+    <Suspense fallback={<span>Loading...</span>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React logo"/>
+            <ul>
+             {
+             routes.map(route => (
+              <li key={route.name}>
+              <NavLink to={route.to} className={({isActive}) => isActive ? 'nav-active' : ''}>
+                {route.name}
+              </NavLink>
+              </li>
+              ))
+            }
+            </ul>
+          </nav>
 
-        <Routes>
-          {routes.map(route => (
-            <Route key={route.name} path={route.path} element={<route.Component/>} />
-            ))
-          }
-          <Route path="/*" element={<Navigate to='/lazy1' replace />} />   
-       </Routes>
-      </div>
-    </BrowserRouter>
+          <Routes>
+            {routes.map(route => (
+              <Route key={route.name} path={route.path} element={<route.Component/>} />
+              ))
+            }
+            <Route path="/*" element={<Navigate to='/lazy1' replace />} />   
+         </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   )
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,25 @@
+import { lazy, LazyExoticComponent } from 'react';
 import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
+type JSXComponent = () => JSX.Element
 interface Route {
   to: string,
   path: string,
-  Component: () => JSX.Element,
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent,
   name: string
 }
+
+//lazy es un método propio de React que hay que importar.
+const Lazy1 = lazy(() => import( /* webpackChunkName:"Lazyload1" */'../01-lazyload/pages/LazyPage1'));
+const lazy2= lazy(() => import( /* webpackChunkName:"Lazyload2" */'../01-lazyload/pages/LazyPage2'));
+const lazy3 = lazy(() => import( /* webpackChunkName:"Lazyload3" */'../01-lazyload/pages/LazyPage3'));
+
+
 export const routes: Route[] = [
   {
     to: '/lazy1',
     path: 'lazy1',
-    Component: LazyPage1,
+    Component: Lazy1,
     name: 'lazy-1'
   },
   {
